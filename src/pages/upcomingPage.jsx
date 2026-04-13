009604c22a720a7d0eff69b8e7d3267b41764790
Atupye Tweve <atutweve64@gmail.com>
2026-04-13 23:58:53 +0300

    modified upcoming page

--- src/pages/upcomingPage.jsx
+++ src/pages/upcomingPage.jsx
@@ -1,16 +1,43 @@
-
 const UpcomingPage = () => {
-  return 
-  <>
-    <div>
-      <div>
+  return (
+    <>
+      <div className ="">
         <div>
-          <h1>Upcoming</h1>
-          <p>Curating your focus for the week ahead.</p>
+          <div>
+            <h1>Upcoming</h1>
+            <p>Curating your focus for the week ahead.</p>
+          </div>
+          <div>Calender part</div>
         </div>
+
+        <p>Tomorrow<span><hr></hr>March 13</span></p>
+        <div>
+          <div>
+            <div></div>
+            <h2>Quarterly Strategy Alignment</h2>
+            <p>helo dcjcdcbhd</p>
+            <div>
+              <div>profiles</div>
+              <div>ratingd</div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <div></div>
+            <h2>Design System Audit</h2>
+            <p>helo dcjcdcbhd</p>
+            <div>
+              <div>profiles</div>
+              <div>ratingd</div>
+            </div>
+          </div>
+        </div>
+
       </div>
-    </div>
-  </>;
+    </>
+  )
 };
 
 export default UpcomingPage;
