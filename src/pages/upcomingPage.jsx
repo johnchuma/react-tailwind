3b15d67eae9498d8bf192165a56ac115f3f4e7e9
Atupye Tweve <atutweve64@gmail.com>
2026-04-13 23:39:05 +0300

    modified upcoming page

--- src/pages/upcomingPage.jsx
+++ src/pages/upcomingPage.jsx
@@ -1,7 +1,15 @@
+
 const UpcomingPage = () => {
   return 
   <>
-    
+    <div>
+      <div>
+        <div>
+          <h1>Upcoming</h1>
+          <p>Curating your focus for the week ahead.</p>
+        </div>
+      </div>
+    </div>
   </>;
 };
 
