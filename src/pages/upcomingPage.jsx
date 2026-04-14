be9fb2652a3c7f76b908a78f51f1f5860630ec08
Atupye Tweve <atutweve64@gmail.com>
2026-04-14 23:49:11 +0300

    modified upcoming page

--- src/pages/upcomingPage.jsx
+++ src/pages/upcomingPage.jsx
@@ -1,5 +1,6 @@
-import { PiDotsThreeOutlineFill } from "react-icons/pi";
+import { PiDotFill, PiDotsThreeOutlineFill } from "react-icons/pi";
 import { HiClock, HiOutlineUsers } from "react-icons/hi2";
+import { MdOutlineLink } from "react-icons/md";
 
 const UpcomingPage = () => {
   return (
@@ -20,7 +21,7 @@ const UpcomingPage = () => {
         </div>
 
         <div className="flex justify-between gap-5">
-          <div className="bg-white rounded-md border-l-red-800 border-l-5 p-2 space-y-2">
+          <div className="bg-white rounded-md border-l-red-800 border-l-5 p-2 space-y-5 py-5">
             <div className="flex justify-between">
               <p className="text-sm bg-red-300 rounded-full px-2 font-bold text-red-900">URGENT</p>
               <PiDotsThreeOutlineFill className="text-neutral/40" />
@@ -38,7 +39,7 @@ const UpcomingPage = () => {
             </div>
           </div>
 
-          <div className="bg-primary/10 p-2 space-y-2 rounded-md">
+          <div className="bg-primary/10 p-2 space-y-5 py-5 rounded-md">
             <div className="flex justify-between">
               <p className="text-sm bg-green-300 rounded-full px-2 font-bold">INTERNAL</p>
               <PiDotsThreeOutlineFill className="text-neutral/40" />
@@ -46,10 +47,10 @@ const UpcomingPage = () => {
             <h2 className="font-bold">Design System Audit</h2>
             <p className="text-neutral/90 text-sm">Ensure all shared components align with the Axiom Indigo specifications.</p>
             <div className="flex justify-between">
-              <div className="bg-white/60 px-2 rounded-full">Project: UI refresh</div>
-              <div>
-                <></>
-                <p>2 Docs</p>
+              <div className="bg-white/60 px-2 rounded-full font-semibold text-neutral text-sm">Project: UI refresh</div>
+              <div className="flex justify-center items-center ">
+                <MdOutlineLink />
+                <p className="font-semibold text-sm text-neutral">2 Docs</p>
               </div>
             </div>
           </div>
@@ -61,23 +62,25 @@ const UpcomingPage = () => {
           <span className="text-neutral/40 text-sm font-bold">March 16-17</span>
         </div>
 
-        <div>
-          <div>
-            <div></div>
-            <h2>Personal Portfolio Revision</h2>
-            <p>helo dcjcdcbhd</p>
-            <div>
-              <div>profiles</div>
-              <div>ratingd</div>
+        <div className="flex gap-5">
+          <div className="bg-white p-2 space-y-2 rounded-md pb-5">
+            <div className="flex items-center">
+              <PiDotFill className="text-red-950" />
+              <p className="text-red-950 text-sm font-bold">IN PROGRESS</p>
+            </div>
+            <h2 className="font-bold text-lg">Personal Portfolio Revision</h2>
+            <p className="text-neutral">Updating the case studies to reflect the new 
+              curated approach to digital design and user narrative.</p>
+            <div className="flex gap-5 items-center">
+              <div className="bg-primary/20 px-2 py-1 font-bold rounded-md text-sm text-primary">Continue Focus</div>
+              <p className="italic text-sm text-neutral">Next step: Finalize mobile views</p>
             </div>
           </div>
-        </div>
-
-        <div>
-          <div>
+        
+          <div className="bg-primary/10 rounded-md text-center pt-20">
             <div></div>
-            <h2>Rest & Restore</h2>
-            <p>helo dcjcdcbhd</p>
+            <h2 className="font-bold">Rest & Restore</h2>
+            <p className="text-sm">No tasks scheduled for Sunday afternoon.</p>
           </div>
         </div>
 
